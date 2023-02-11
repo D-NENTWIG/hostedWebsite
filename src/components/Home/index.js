0c5ab4dec6383d247dcce9bd37aa933ccbca9c05
D-NENTWIG <david.nentwig.21@ucl.ac.uk>
2023-02-11 17:35:52 +0000

    added homepage elements

---
+++ src/components/Home/index.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/NENTWIG.png';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['D','a','v','i','d']
+    const jobArray = ['S','o','f','t','w','a','r','e',' ','E','n','g','i','n','e','e','r',]
+
+    useEffect(() => {
+        return () => setTimeout(() => {
+            setLetterClass('text-animate-hover')
+    }, 4000)
+    }, [])
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}> </span>
+                <span className={`${letterClass} _14`}>I</span>
+                <span className={`${letterClass} _15`}>'m</span>
+                    
+                <br /> 
+
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={16} />
+
+                <img src={LogoTitle} alt="developer" />
+                <br/>
+
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={21} />
+
+                </h1>
+                <h2>Student at UCL | Computer Science</h2>
+                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home
