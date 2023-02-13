df2fd429a41bf05e1296cfb8841f0af6acb6588c
D-NENTWIG <david.nentwig.21@ucl.ac.uk>
2023-02-13 22:25:33 +0000

    removed reloading on contact submit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -27,7 +27,6 @@ const Contact = () => {
             .then(
                 () =>{
                     alert('Message successfully sent')
-                    window.location.reload(false)
                 },
                 () => {
                     alert('Failed to send the message, please try again.')
